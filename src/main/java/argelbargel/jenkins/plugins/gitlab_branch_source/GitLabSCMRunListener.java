85dbce02ab22941f6b02e10eb1840e8e9d59180c
Herbert Weigert Venancio <herbert.venancio@objective.com.br>
2019-02-19 15:35:07 -0300

    [TOOLS-2336] bugfix notify gitlab after first run

--- src/main/java/argelbargel/jenkins/plugins/gitlab_branch_source/GitLabSCMRunListener.java
+++ src/main/java/argelbargel/jenkins/plugins/gitlab_branch_source/GitLabSCMRunListener.java
@@ -5,19 +5,33 @@ import argelbargel.jenkins.plugins.gitlab_branch_source.actions.GitLabSCMAcceptM
 import argelbargel.jenkins.plugins.gitlab_branch_source.actions.GitLabSCMCauseAction;
 import argelbargel.jenkins.plugins.gitlab_branch_source.actions.GitLabSCMHeadMetadataAction;
 import argelbargel.jenkins.plugins.gitlab_branch_source.actions.GitLabSCMPublishAction;
+import argelbargel.jenkins.plugins.gitlab_branch_source.heads.GitLabSCMHead;
+import argelbargel.jenkins.plugins.gitlab_branch_source.heads.GitLabSCMMergeRequestHead;
 import hudson.Extension;
+import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.model.listeners.RunListener;
+import jenkins.plugins.git.AbstractGitSCMSource;
+import jenkins.scm.api.SCMSource;
 
 import javax.annotation.Nonnull;
 
 import static hudson.model.Result.SUCCESS;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jenkinsci.plugins.workflow.multibranch.BranchJobProperty;
+
 
 @SuppressWarnings("unused")
 @Extension
 public class GitLabSCMRunListener extends RunListener<Run<?, ?>> {
+
+    private static final Logger LOGGER = Logger.getLogger(GitLabSCMRunListener.class.getName());
+
     @Override
     public void onStarted(Run<?, ?> build, TaskListener listener) {
         GitLabSCMHeadMetadataAction metadata = getMetadataAction(build);
@@ -48,8 +62,34 @@ public class GitLabSCMRunListener extends RunListener<Run<?, ?>> {
 
     private GitLabSCMHeadMetadataAction getMetadataAction(Run<?, ?> build) {
         GitLabSCMHeadMetadataAction metadata = build.getAction(GitLabSCMHeadMetadataAction.class);
-        if (metadata == null) {
-            metadata = build.getParent().getAction(GitLabSCMHeadMetadataAction.class);
+        if (metadata != null)
+            return metadata;
+
+        Job<?, ?> job = build.getParent();
+        metadata = build.getParent().getAction(GitLabSCMHeadMetadataAction.class);
+        if(metadata != null)
+            return metadata;
+
+        GitLabSCMSource src = (GitLabSCMSource) SCMSource.SourceByItem.findSource(job);
+        if(src == null)
+            return null;
+
+        BranchJobProperty branchJob = job.getProperty(BranchJobProperty.class);
+        GitLabSCMHead head = (GitLabSCMHead) branchJob.getBranch().getHead();
+        AbstractGitSCMSource.SCMRevisionImpl revision = head.getRevision();
+        String name = head.getName();
+        GitLabSCMHead branch = head instanceof GitLabSCMMergeRequestHead ? ((GitLabSCMMergeRequestHead) head).getSource() : head;
+        int projectId = branch.getProjectId();
+        String branchName = branch.getName();
+        String hash = revision.getHash();
+        String url = src.getProject().getWebUrl() + "/" + "commits/" + hash;
+        metadata = new GitLabSCMHeadMetadataAction(name, projectId, branchName, hash, url);
+        try {
+            job.addAction(metadata);
+            job.save();
+        } catch (IOException ex) {
+            // ignore error
+            LOGGER.log(Level.SEVERE, "cannot save job", ex);
         }
 
         return metadata;
